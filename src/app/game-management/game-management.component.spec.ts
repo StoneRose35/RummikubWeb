ce305a82a6ed7de6e828d5250bf7f8007e6502ee
Philipp Fuerholz <fuerholz@gmx.ch>
2020-12-29 23:17:35 +0100

    corrected tests

--- src/app/game-management/game-management.component.spec.ts
+++ src/app/game-management/game-management.component.spec.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { GameManagementComponent } from './game-management.component';
 import {MatSnackBarModule, MatSnackBarConfig} from '@angular/material/snack-bar';
 import { MatDialogModule } from '@angular/material/dialog';
 import {DragDropModule} from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GameService } from '../game.service';
 
 
 describe('GameManagementComponent', () => {
@@ -13,7 +15,7 @@ describe('GameManagementComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ GameManagementComponent ],
-      imports: [MatSnackBarModule, MatDialogModule, DragDropModule],
+      imports: [MatSnackBarModule, MatDialogModule, DragDropModule, HttpClientModule, BrowserAnimationsModule],
       providers: [MatSnackBarConfig]
     })
     .compileComponents();
--- src/app/games-overview/games-overview.component.spec.ts
+++ src/app/games-overview/games-overview.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {MatSnackBarModule, MatSnackBarConfig} from '@angular/material/snack-bar';
 import { GamesOverviewComponent } from './games-overview.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('GamesOverviewComponent', () => {
   let component: GamesOverviewComponent;
@@ -8,7 +12,9 @@ describe('GamesOverviewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GamesOverviewComponent ]
+      declarations: [ GamesOverviewComponent ],
+      imports: [MatSnackBarModule, HttpClientModule, MatDialogModule, RouterTestingModule],
+      providers: [MatSnackBarConfig, MatDialog]
     })
     .compileComponents();
   });
